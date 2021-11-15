fb77b6e04b01080cd761484c092fe36859582e44
emptymonkey <pseudorandom@gmail.com>
2021-11-15 01:09:11 -0800

    Fixed bugs.

--- lindir2windir.sh
+++ lindir2windir.sh
@@ -7,8 +7,42 @@
 # from one environment to the other.
 #
 
+SED=`which sed`
+TR=`which tr`
+ECHO=`which echo`
+REALPATH=`which realpath`
+GREP=`which grep`
+
+if [ -z $SED -o -z $TR -o -z $ECHO -o -z $REALPATH ]; then
+	echo 'blah'
+	exit 1
+fi
+
+usage() {
+	$ECHO "Usage: $0 [-e] [-h]" 1>&2
+	$ECHO "\t-e:\tEscape mode. All [^a-zA-Z0-9] characters are escaped."
+	$ECHO "\t-h:\tThis message."
+	exit 1
+}
+
+ESCAPE=0
+while getopts "eh" o; do
+	case "${o}" in
+		e)
+			ESCAPE=1
+			;;
+		h)
+			usage
+			;;
+		*)
+			usage
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
 # Grab the name of this program.
-PROGRAM=`echo $0 | sed "s/.*\///"`
+PROGRAM=`$ECHO $0 | $SED "s/.*\///"`
 
 # If invoked as "windir2lindir.sh" then translate windows path name to linux.
 # Otherwise, assume this is the linux to windows invocation.
@@ -21,21 +55,26 @@ fi
 for i in "$@"; do
 
 	# Windows to Linux mode.
-	if [ "$WIN_MODE" -eq "1" ]; then
-		DRIVE=`echo $i | sed "s/^\([A-Z]\).*/\1/" | tr '[:upper:]' '[:lower:]'`
-		PATH=`echo $i | sed "s/^[A-Z]://"	| sed "s/\\\\\\/\//g" | sed "s/\([^a-zA-Z0-9/]\)/\\\\\\\\\1/g"`
-		echo "/mnt/$DRIVE$PATH"
+	if [ $WIN_MODE -eq 1 ]; then
+		DRIVE=`$ECHO $i | $SED "s/^\([A-Z]\).*/\1/" | $TR '[:upper:]' '[:lower:]'`
+		PATH=`$ECHO $i | $SED "s/^[A-Z]://"	| $SED "s/\\\\\\/\//g" | $SED "s/\([^a-zA-Z0-9/]\)/\\\\\\\\\1/g"`
+		$ECHO "/mnt/$DRIVE$PATH"
 
 	# Linux to Windows mode.
-	else
-		if [ `echo $i | grep '^.'` ]; then
-			i=`realpath $i`
+else
+	$ECHO $i | $GREP '^\.' 1>/dev/null
+	if [ $? -eq 0 ]; then
+		i=`$REALPATH "$i"`
+	fi
+	$ECHO $i | $GREP '/mnt/[a-z]/' 1>/dev/null
+	if [ $? -eq 0 ]; then
+		DRIVE=`$ECHO $i | $SED "s/^\/mnt\/\([a-z]\).*/\1/" | $TR '[:lower:]' '[:upper:]'`
+		PATH=`$ECHO $i | $SED "s/^\/mnt\/[a-z]//"`
+		if [ $ESCAPE -eq 1 ]; then
+			PATH=`$ECHO $PATH | $SED "s/\([^a-zA-Z0-9/]\)/\\\\\\\\\1/g"`
 		fi
-		if [ `echo $i | grep '/mnt/[a-z]/'` ]; then
-			DRIVE=`echo $i | sed "s/^\/mnt\/\([a-z]\).*/\1/" | tr '[:lower:]' '[:upper:]'`
-			PATH=`echo $i | sed "s/^\/mnt\/[a-z]//" | sed "s/\\\\\\\\\([^a-zA-Z0-9/]\)/\1/g"`
-			echo "$DRIVE:$PATH"
-		fi
+		$ECHO "$DRIVE:$PATH"
+	fi
 	fi
 done
 
