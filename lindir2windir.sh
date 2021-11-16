fe75ed5f2b09ba1026bd8a53e222a06d845dc41e
emptymonkey <pseudorandom@gmail.com>
2021-11-15 16:46:31 -0800

    Changed variable names that were causing chaos.

--- lindir2windir.sh
+++ lindir2windir.sh
@@ -45,14 +45,11 @@ fi
 # We'll take as many directory names on the command line as you want.
 for i in "$@"; do
 
-	# XXX If we don't reset the path every loop, then shit breaks. WTActualF?!
-	export PATH="/bin:/usr/bin:/usr/local/bin"
-
 	# Windows to Linux mode.
 	if [ $WIN_MODE -eq 1 ]; then
 		DRIVE=`echo $i | sed "s/^\([A-Z]\).*/\1/" | tr '[:upper:]' '[:lower:]'`
-		PATH=`echo $i | sed "s/^[A-Z]://"	| sed "s/\\\\\\/\//g" | sed "s/\([^a-zA-Z0-9/.]\)/\\\\\\\\\1/g"`
-		echo "/mnt/$DRIVE$PATH"
+		NEW_PATH=`echo $i | sed "s/^[A-Z]://"	| sed "s/\\\\\\/\//g" | sed "s/\([^a-zA-Z0-9/.]\)/\\\\\\\\\1/g"`
+		echo "/mnt/$DRIVE$NEW_PATH"
 
 	# Linux to Windows mode.
 	else
@@ -64,14 +61,14 @@ for i in "$@"; do
 		echo "$ABSOLUTE_PATH | egrep '/mnt/[a-z]/'" 1>/dev/null
 		if [ $? -eq 0 ]; then
 			DRIVE=`echo $ABSOLUTE_PATH | sed "s/^\/mnt\/\([a-z]\).*/\1/" | tr '[:lower:]' '[:upper:]'`
-			PATH=`echo $ABSOLUTE_PATH | sed "s/^\/mnt\/[a-z]//" | sed "s/\//\\\\\\\\/g"`
+			NEW_PATH=`echo $ABSOLUTE_PATH | sed "s/^\/mnt\/[a-z]//" | sed "s/\//\\\\\\\\/g"`
 			if [ $ESCAPE -eq 1 ]; then
-				PATH=`echo $PATH | sed "s/\([^a-zA-Z0-9/\\]\)/\\\\\\\\\1/g"`
+				NEW_PATH=`echo $NEW_PATH | sed "s/\([^a-zA-Z0-9/\\]\)/\\\\\\\\\1/g"`
 			fi
-			if [ -z "$PATH" ]; then
-				PATH="\\\\"
+			if [ -z "$NEW_PATH" ]; then
+				NEW_PATH="\\\\"
 			fi
-			echo "$DRIVE:$PATH"
+			echo "$DRIVE:$NEW_PATH"
 		fi
 	fi
 done
