71898625a098a755f09a4278a26e88276742651b
emptymonkey <pseudorandom@gmail.com>
2021-11-15 21:48:05 -0800

    Tracked down /fixed /bin/sh built in echo bug.

--- lindir2windir.sh
+++ lindir2windir.sh
@@ -2,6 +2,12 @@
 
 export PATH="/bin:/usr/bin:/usr/local/bin"
 
+# sh has a builtin echo that always translates control characters.
+# That means that in the case of 'C:\one\two\foo\bar' the "\t" in "\two" 
+# is treated as a tab character. This results in "C:\one	wo\foo\bar".
+# This is not what we want.
+ECHO=`which echo`" -E"
+
 # lindir2windir.sh
 #		-	ln -s lindir2windir.sh windir2lindir.sh
 #
@@ -10,7 +16,7 @@ export PATH="/bin:/usr/bin:/usr/local/bin"
 #
 
 # Grab the name of this program.
-PROGRAM=`echo $0 | sed "s/.*\///"`
+PROGRAM=`$ECHO $0 | sed "s/.*\///"`
 
 # If invoked as "windir2lindir.sh" then translate windows path name to linux.
 # Otherwise, assume this is the linux to windows invocation.
@@ -24,23 +30,22 @@ for i in "$@"; do
 
 	# Windows to Linux mode.
 	if [ $WIN_MODE -eq 1 ]; then
-		DRIVE=`echo $i | sed "s/^\([A-Z]\).*/\1/" | tr '[:upper:]' '[:lower:]'`
-		NEW_PATH=`echo $i | sed "s/^[A-Z]://"	| sed "s/\\\\\\/\//g" | sed "s/\([^a-zA-Z0-9/.]\)/\\\\\\\\\1/g"`
-		echo "/mnt/$DRIVE$NEW_PATH"
+		DRIVE=`$ECHO $i | sed "s/^\([A-Z]\).*/\1/" | tr '[:upper:]' '[:lower:]'`
+		NEW_PATH=`$ECHO $i | sed "s/^[A-Z]://"	| sed "s/\\\\\\/\//g" | sed "s/\([^a-zA-Z0-9/.]\)/\\\\\\\\\1/g"`
+		$ECHO "/mnt/$DRIVE$NEW_PATH"
 
 	# Linux to Windows mode.
 	else
-		ABSOLUTE_PATH=`echo $i | tr -d '\\\\'`
+		ABSOLUTE_PATH=`$ECHO $i | tr -d '\\\\'`
 		ABSOLUTE_PATH=`realpath "$ABSOLUTE_PATH"`
-		echo "$ABSOLUTE_PATH | egrep '/mnt/[a-z]/'" 1>/dev/null
+		$ECHO "$ABSOLUTE_PATH | egrep '/mnt/[a-z]/'" 1>/dev/null
 		if [ $? -eq 0 ]; then
-			DRIVE=`echo $ABSOLUTE_PATH | sed "s/^\/mnt\/\([a-z]\).*/\1/" | tr '[:lower:]' '[:upper:]'`
-			NEW_PATH=`echo $ABSOLUTE_PATH | sed "s/^\/mnt\/[a-z]//"`
-			NEW_PATH=`echo $NEW_PATH | sed "s/\/\(.\)/\\\\\\\\\\\\\\\\\1/g"`
+			DRIVE=`$ECHO $ABSOLUTE_PATH | sed "s/^\/mnt\/\([a-z]\).*/\1/" | tr '[:lower:]' '[:upper:]'`
+			NEW_PATH=`$ECHO $ABSOLUTE_PATH | sed "s/^\/mnt\/[a-z]//" | tr '/' '\\\\'`
 			if [ -z "$NEW_PATH" ]; then
-				NEW_PATH="\\\\"
+				NEW_PATH="\\"
 			fi
-			echo "$DRIVE:$NEW_PATH"
+			$ECHO "$DRIVE:$NEW_PATH"
 		fi
 	fi
 done
